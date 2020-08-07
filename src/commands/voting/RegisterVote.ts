04205f0388ed85192c09c377543181537fac9e1c
Fraser McCallum <fraser@themccallums.net.nz>
2020-08-07 20:43:21 +1200

    added registry for members registering with emails

---
+++ src/commands/voting/RegisterVote.ts
@@ -0,0 +1,52 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message } from "discord.js";
+import { Bot } from "../../Bot";
+import { UserSettingsModel } from "../../database/models/UserSettings/UserSettings.model";
+
+export default class RegisterVoteCommand extends Command {
+    constructor(client: CommandoClient) {
+        super(client, {
+            name: "registervote",
+            aliases: ["registertovote", "voteregister"],
+            group: "voting",
+            memberName: "registervote",
+            description: "Create an anonymous poll (for administrators).",
+            clientPermissions: ["SEND_MESSAGES"],
+            args: [
+                {
+                    key: "email",
+                    prompt: "Please enter the email you used to sign up for the club.",
+                    type: "string",
+                    validate: (s: string) => s.length < 1000,
+                },
+            ],
+        });
+    }
+
+    async run(message: CommandoMessage, { email }: CommandArguments): Promise<Message> {
+        email = email.toLowerCase();
+        const result = Bot.Get.isValidEmail(email);
+        if (result) {
+            const user = await UserSettingsModel.findOneOrCreate({ userId: message.author.id });
+            if (user.getEmail()) {
+                return message.channel.send("You already have an email associated.");
+            }
+            await user.updateEmail({ email: email });
+            return message.channel.send("You have been registered successfully!");
+        } else {
+            return message.channel.send(
+                "This email is not valid, or is already in use. If you think there is a mistake, please contact an admin."
+            );
+        }
+    }
+}
+
+interface CommandArguments {
+    email: string;
+}
+
+interface Candidate {
+    reaction: string;
+    candidate: string;
+    votes: Set<string>;
+}
---
+++ src/events/handlers/MemberRegister.ts
@@ -0,0 +1,53 @@
+import { BotEvent } from "../Events.interface";
+import { Bot } from "../../Bot";
+import { ServerSettingsModel } from "../../database/models/ServerSettings/ServerSettings.model";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import Log from "../../helpers/Log";
+import { UserSettingsModel } from "../../database/models/UserSettings/UserSettings.model";
+
+/**
+ * Gets list of members from the spreadsheet
+ */
+export default class MemberRegisterCount implements BotEvent {
+    private client: Bot = Bot.Get;
+
+    async start(): Promise<void> {
+        await this.SheetReader();
+        await this.DatabaseReader();
+    }
+
+    private async SheetReader() {
+        if (process.env.MEMBER_SHEET == undefined) {
+            return Log.error("MemberRegister", "No MEMBER_SHEET value provided in env file.");
+        }
+        const credentials = require("../../../client_secret.json");
+        const document = new GoogleSpreadsheet(process.env.MEMBER_SHEET);
+        await document.useServiceAccountAuth(credentials);
+        await document.loadInfo();
+
+        const sheet = document.sheetsByIndex[0];
+        const rows = await sheet.getRows();
+
+        rows.forEach((row) => {
+            const email: string = row["University Email Address"];
+            const name = row["Full Name (AS SHOWN ON ID CARD)"];
+            const uniId = row["ID Number (N/A if non-UoA Student)"];
+            const games = row["What games do you play?"];
+            const studyYear = row["What year of study are you in?"];
+            this.client.addClubMember({
+                email: email.toLowerCase(),
+                name: name,
+                uniId: uniId,
+                games: games,
+                studyYear: studyYear,
+            });
+        });
+    }
+
+    private async DatabaseReader() {
+        const users = await UserSettingsModel.find();
+        users.forEach((user) => {
+            this.client.refreshUserCachePoint(user);
+        });
+    }
+}
