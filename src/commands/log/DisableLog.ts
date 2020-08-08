7083fb10539450d77cc4cf23602d12bdfcc746de
Fraser McCallum <fraser@themccallums.net.nz>
2020-08-09 00:45:10 +1200

    Added log commands for setting/removing log channel

---
+++ src/commands/log/DisableLog.ts
@@ -0,0 +1,22 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message, Channel, TextChannel } from "discord.js";
+import { ServerSettingsModel } from "../../database/models/ServerSettings/ServerSettings.model";
+
+export default class DisableLogCommand extends Command {
+    constructor(client: CommandoClient) {
+        super(client, {
+            name: "disablelog",
+            aliases: ["logoff", "stoplog"],
+            group: "fun",
+            memberName: "disablelog",
+            description: "Replies with a lenny face",
+            userPermissions: ["ADMINISTRATOR"],
+        });
+    }
+
+    async run(message: CommandoMessage): Promise<Message> {
+        const settings = await ServerSettingsModel.findOneOrCreate({ guildId: message.guild.id });
+        await settings.removeLogChannel();
+        return message.channel.send("Logging has been disabled!");
+    }
+}
---
+++ src/commands/log/LogChannel.ts
@@ -0,0 +1,46 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message, Channel, TextChannel } from "discord.js";
+import { ServerSettingsModel } from "../../database/models/ServerSettings/ServerSettings.model";
+
+export default class LogChannelCommand extends Command {
+    constructor(client: CommandoClient) {
+        super(client, {
+            name: "logchannel",
+            aliases: ["log", "logger", "loggingchannel"],
+            group: "fun",
+            memberName: "logchannel",
+            description: "Replies with a lenny face",
+            userPermissions: ["ADMINISTRATOR"],
+            args: [
+                {
+                    key: "logChannel",
+                    prompt: "What would you like to set the log channel to?",
+                    type: "channel",
+                    default: new Channel(client, { type: "text" }),
+                },
+            ],
+        });
+    }
+
+    async run(message: CommandoMessage, { logChannel }: { logChannel: Channel }): Promise<Message> {
+        const settings = await ServerSettingsModel.findOneOrCreate({ guildId: message.guild.id });
+        if (logChannel.id == undefined) {
+            const currentChannelId = await settings.getLogChannel();
+            let currentChannel: string;
+            if (currentChannelId == undefined) {
+                currentChannel = "not set up";
+            } else {
+                currentChannel = message.guild.channels.resolve(currentChannelId)!.name;
+            }
+            return message.channel.send(`This servers log channel is **${currentChannel}**.`);
+        }
+
+        if (logChannel.type != "text") {
+            return message.channel.send("Log channel must be a text channel.");
+        }
+
+        await settings.setLogChannel({ channel: logChannel.id });
+
+        return message.channel.send(`Log channel has been updated to **${(logChannel as TextChannel).name}**`);
+    }
+}
