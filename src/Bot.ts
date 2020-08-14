67a8474226939a1f9e50784bb6e07214a09ac65c
Fraser McCallum <fraser@themccallums.net.nz>
2020-08-15 01:05:08 +1200

    Made the event manager accessible from bot class

--- src/Bot.ts
+++ src/Bot.ts
@@ -6,6 +6,7 @@ import { EventManager } from "./EventManager";
 import { ReactionCollector, MessageCollector as Function, Message } from "discord.js";
 import { ServerSettingsModel, IServerSettingsDocument } from "./database/models/Server/Server.model";
 import { IUserSettingsDocument } from "./database/models/Client/User.model";
+import { EventEmitter } from "events";
 
 dotenv.config();
 export class Bot extends CommandoClient {
@@ -25,6 +26,8 @@ export class Bot extends CommandoClient {
             invite: "https://discord.gg/rwFhQ9V",
             disableMentions: "everyone",
         });
+
+        this.eventManager = new EventManager();
         this.reactionListeners = new Map();
         this.commandListeners = new Map();
         this.guildSettingsCache = new Map();
@@ -103,8 +106,13 @@ export class Bot extends CommandoClient {
         this.commandListeners.delete(key);
     }
 
+    private eventManager: EventManager;
+    public get EventManager() {
+        return this.eventManager;
+    }
+
     private registerEvents(): void {
-        new EventManager().init();
+        this.eventManager.init();
     }
 
     private guildSettingsCache: Map<string, IServerSettingsDocument>;
