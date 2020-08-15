49a6732a6df191933698ea4c2b585cf13185e1b4
Fraser McCallum <fraser@themccallums.net.nz>
2020-08-16 03:30:23 +1200

    added string support for delimit function

--- src/helpers/Utility.ts
+++ src/helpers/Utility.ts
@@ -1,4 +1,4 @@
-export function delimit(x: number) {
+export function delimit(x: number | string) {
     var parts = x.toString().split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return parts.join(".");
